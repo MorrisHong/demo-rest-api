e87fc477e3f6c9c53201984218afd3fbf5c44e5d
gracelove <govlmo91@gmail.com>
2020-02-14 19:21:51 +0900

    init

---
+++ src/test/java/kr/gracelove/demorestapi/DemoRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.gracelove.demorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
